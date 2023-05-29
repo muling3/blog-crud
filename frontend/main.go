276f8659fd2313a6017d5bbf71092196f6c0a43c
AlexanderMuli <mulingealexmuli@gmail.com>
2023-05-29 17:27:49 +0300

    index.html :: static

--- frontend/main.go
+++ frontend/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
 
 func main() {
-	fmt.Println("app started successfully")
+
+	//engine
+	engine := html.New("./views", ".html")
+
+	//inityialising app
+	app := fiber.New(fiber.Config{
+		Views: engine,
+		ViewsLayout: "layouts/main",
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Render("index", fiber.Map{
+			"Title": "Hello Index",
+		})
+	})
+
+	//serving static files
+	app.Static("/", "./public")
+
+	log.Fatal(app.Listen(":4000"))
+
 }
